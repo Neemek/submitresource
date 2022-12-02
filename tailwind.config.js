142780ec7edadfb2b615ef7098003e528c68ba7d
Neemek <sindodeg@icloud.com>
2022-12-02 23:30:01 +0100

    Tailwind and `npm` scripts

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/routes/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    fontFamily: {
+      'nemok': ['"Segoe UI"', '"Trebuchet MS"', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
