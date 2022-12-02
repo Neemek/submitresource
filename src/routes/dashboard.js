3537b2f77ce3f0fc3f8fdb911fe7221971874c94
Neemek <sindodeg@icloud.com>
2022-12-02 23:30:35 +0100

    Auto-redirect and asynchronous token refreshing

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,13 +1,24 @@
-function checkIfAuthorized() {
+function checkIfAuthorized(redirect = false) {
     fetch('/api/oauth2/hasauthorized')
         .then(async res => {
-            document.getElementById('auth-span').innerHTML = res.status == 400 ?
-                `Not authenticated. <a href="${await ((await fetch('/api/oauth2/authurl')).text())}" class="re-auth">Reauthenticate?</a>` : ((res.status == 200 ? '<span class="auth-text">Authenticated.</span>' : '<span class="auth-text">Authenticated. (No access_token)</span>') + ' <a class="recheck" href="javascript:void();" onclick="checkIfAuthorized(); return false;" style="display: inline;">Recheck?</a>');
+            if (res.status == 400) {
+                let authUrl = await ((await fetch('/api/oauth2/authurl')).text());
+
+                if (redirect) {
+                    window.location.replace(authUrl);
+                } else {
+                    document.getElementById('auth-span').innerHTML = `Not authenticated. <a href="${authUrl}" class="re-auth">Reauthenticate?</a>`;
+                }
+            } else {
+                document.getElementById('auth-span').innerHTML =
+                    ((res.status == 200 ? '<span class="text-amber-700">Authenticated.</span>'
+                        : '<span class="auth-text">Authenticated. (No access_token)</span>') + ' <a class="recheck" href="javascript:void();" onclick="checkIfAuthorized(); return false;" style="display: inline;">Recheck?</a>');
+            }
         })
         .catch(console.error);
 }
 
-checkIfAuthorized()
+checkIfAuthorized(true)
 
 function joinServer() {
     let id = document.getElementById('guildId').value;
@@ -23,4 +34,9 @@ function getUserInfo() {
     fetch("/api/oauth2/user")
         .then(res => res.json())
         .then(json => document.getElementById('user-info').innerText = JSON.stringify(json).replace(/,/g, ',\n').replace(/:/g, ': '));
+}
+
+async function asyncRefreshTokens() {
+    await fetch('/api/oauth2/refresh?redirect=true')
+    checkIfAuthorized()
 }
